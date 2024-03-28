01c2b34c935162de0160fa594d82f3f7594767fe
ZathuraONG <sakupenhell420@gmail.com>
2024-03-27 20:17:56 -0800

    second commit

---
+++ day 1/homework/classwork/lesson1.py
@@ -0,0 +1,7 @@
+name = "vako"
+surname = "sisauri"
+age = "15"
+height = "178"
+weight = "68.5"
+
+print("me var" + " " + name + " " + surname + " " + "me var" + " " + str(age) + " " + "wlis, simagleshi var" + " " + str(height) + " " + "santimetri da viwoni" + " " + str(weight) + " " +"kilograms")
--- day 1/lesson1.py
+++
@@ -1,7 +0,0 @@
-name = "vako"
-surname = "sisauri"
-age = "15"
-height = "178"
-weight = "68.5"
-
-print("me var" + " " + name + " " + surname + " " + "me var" + " " + str(age) + " " + "wlis" + " " + "simagleshi var" + " " + str(height) + " " + "santimetri" + " " + "da viwoni" + " " + str(weight) + " " +"kilograms")

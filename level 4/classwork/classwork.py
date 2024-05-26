1e08b8423718bfb0dc7c25a7f51c2d21c20bdef8
ZathuraONG <sakupenhell420@gmail.com>
2024-05-25 17:14:21 -0800

    fourth commit

---
+++ level 4/classwork/classwork.py
@@ -0,0 +1,17 @@
+num = 5
+num = num + 1
+
+num1 = 7
+num1 = num1 + 1
+
+print(num)
+print(num1)
+
+#გადაწერა ნიშნავს ცვლადისთვის ახალი მნიშვნელობის მინიჭებას
+
+balance = 10
+discount = 0.8
+summary = 10 * 0.8
+
+print(summary)
+

c00a7e73a8f8a4f7e4b7a512d4e5b21611c06781
ZathuraONG <sakupenhell420@gmail.com>
2024-07-21 07:55:34 -0800

    19th commit

---
+++ level 18/homework/homework.py
@@ -0,0 +1,53 @@
+#task 2
+def add_numbers(a, b):
+    return a + b
+
+num1 = 5
+num2 = 3
+result = add_numbers(num1, num2)
+print(result)
+
+#task 3
+
+def multiply_three_numbers(a, b, c):
+    return a * b * c
+
+num1 = 4
+num2 = 5
+num3 = 6
+result = multiply_three_numbers(num1, num2, num3)
+print(result)
+
+
+#task 4
+
+def greet_person(first_name, last_name):
+    return f"Hello, {first_name} {last_name}! Nice to meet you."
+
+first_name = "Vako"
+last_name = "Sisauri"
+greeting = greet_person(first_name, last_name)
+print(greeting)
+
+#task 5
+
+def add_numbers(a, b):
+    return a + b
+
+num1 = 10
+num2 = 20
+result = add_numbers(num1, num2)
+
+print(result)
+
+#task 6
+
+def multiply_three_numbers(a, b, c):
+    return a * b * c
+
+num1 = 4
+num2 = 5
+num3 = 6
+product_result = multiply_three_numbers(num1, num2, num3)
+
+print(product_result)
